ab6ca4dc567744fd3be8daa6996771d99b041bc1
shihao-hub <2958017271@qq.com>
2024-09-10 01:58:51 +0800

    general commit

---
+++ mongodb_exercises/mongoengine_test.py
@@ -0,0 +1,14 @@
+import pymongo
+import mongoengine as m
+
+
+# SLAP
+
+
+# 获取接口数据
+# 将接口返回的数据存入 json，可以尝试分片
+# 利用 mongodb json 导入功能，将 json 导入并存入 Mongodb 数据库
+# 通过 Mongodb 查询我所需要的数据
+
+class CombatPlatformModel:
+    pass
