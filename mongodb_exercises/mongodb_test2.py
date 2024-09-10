0206200d60ddbea98359569d050f66237e5b46ad
shihao-hub <2958017271@qq.com>
2024-09-11 00:47:57 +0800

    general commit

---
+++ mongodb_exercises/mongodb_test2.py
@@ -0,0 +1,76 @@
+import json
+import pprint
+import time
+
+import pymongo
+import mongoengine as engine
+
+TODAY = time.strftime("%Y-%m-%d", time.localtime())
+
+conn = pymongo.MongoClient(host="localhost", port=27017)
+
+db = conn.get_database("combat_platform")
+
+with open("./mongodb_test2.json", "r", encoding="utf-8") as file:
+    file_data = json.load(file)
+
+co = db.get_collection(TODAY)
+co.drop()
+co.insert_one(file_data)
+
+engine.connect("combat_platform")
+
+
+class Review(engine.Document):  # EmbeddedDocument
+    user = engine.StringField(required=True)
+    comment = engine.StringField(required=True)
+    rating = engine.IntField(required=True)
+    date = engine.DateField(required=True)
+
+    meta = dict(collection="co_review")
+
+
+class Ratings(engine.Document):  # EmbeddedDocument
+    average = engine.FloatField(required=True)
+    reviews = engine.ListField(engine.ReferenceField(Review))  # EmbeddedDocumentField
+
+    meta = dict(collection="co_ratings")
+
+    def __init__(self, *args, **values):
+        if "reviews" in values:
+            data = values.pop("reviews")
+            res = []
+            for d in data:
+                inst = Review(**d)
+                inst.save()
+                res.append(inst)
+            values["reviews"] = res
+        super().__init__(*args, **values)
+
+
+class Product(engine.Document):
+    id = engine.StringField(primary_key=True, required=True)
+    title = engine.StringField(required=True)
+    author = engine.StringField(required=True)
+    genres = engine.ListField(engine.StringField(), required=True)
+    price = engine.FloatField(required=True)
+    inStock = engine.BooleanField(required=True)
+    ratings = engine.ReferenceField(Ratings, required=True)  # EmbeddedDocumentField
+
+    meta = dict(collection="co_product")
+
+    def __init__(self, *args, **values):
+        if "ratings" in values:
+            data = values.pop("ratings")
+            inst = Ratings(**data)
+            inst.save()
+            values["ratings"] = inst
+        super().__init__(*args, **values)
+
+
+print("products:\n")
+products = co.find_one({}, {"store.products": 1}).get("store").get("products")
+for e in products:
+    product = Product(**e)
+    print(pprint.pformat(e), flush=True)
+    product.save()
--- mongodb_exercises/mongoengine_test.py
+++ mongodb_exercises/mongoengine_test.py
@@ -1,10 +1,12 @@
+import time
+
 import pymongo
 import mongoengine as m
 
+conn = pymongo.MongoClient(host="localhost", port=27017)
+
 
 # SLAP
-
-
 # 获取接口数据
 # 将接口返回的数据存入 json，可以尝试分片
 # 利用 mongodb json 导入功能，将 json 导入并存入 Mongodb 数据库
@@ -12,3 +14,62 @@ import mongoengine as m
 
 class CombatPlatformModel:
     pass
+
+
+test_data = {
+    "data": {
+        "level1": {
+            "level2": {
+                "level3": {
+                    "level4": {
+                        "end": True,
+                        "users": [
+                            {
+                                "id": 1,
+                                "name": "Alice",
+                                "age": 30,
+                                "email": "alice@example.com",
+                                "address": {
+                                    "street": "123 Main St",
+                                    "city": "Wonderland",
+                                    "zip": "12345"
+                                }
+                            },
+                            {
+                                "id": 2,
+                                "name": "Bob",
+                                "age": 25,
+                                "email": "bob@example.com",
+                                "address": {
+                                    "street": "456 High St",
+                                    "city": "Wonderland",
+                                    "zip": "12345"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+def take_today_as_key():
+    return time.strftime("%Y%m%d", time.localtime())
+
+
+db = conn.get_database("combat_platform")
+co_skeleton = db.get_collection(take_today_as_key() + ":" + "skeleton")
+co_level4_users = db.get_collection(take_today_as_key() + ":" + "level4_users")
+co_skeleton.drop()
+co_level4_users.drop()
+print("集合 :skeleton 和 :level4_users 清理完成")
+
+skeleton = test_data.get("data")
+level4 = skeleton.get("level1").get("level2").get("level3").get("level4")
+level4_users = level4.pop("users")
+co_level4_users.insert_many(level4_users)
+level4["users"] = [e.get("_id") for e in co_level4_users.find()]
+co_skeleton.insert_one(skeleton)
+
